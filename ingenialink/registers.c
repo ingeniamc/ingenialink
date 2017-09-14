d9b98e3fffad00b9549009d900706ec99502e7c4
Gerard Marull-Paretas <gmarull@ingeniamc.com>
2017-09-14 11:49:14 +0200

    registers: remove dot structure initialization so that compilers not fully supporting C99 work

--- ingenialink/registers.c
+++ ingenialink/registers.c
@@ -30,77 +30,62 @@
 
 /* Vendor specific (Ingenia) */
 const il_reg_t IL_REG_MOTPARAM_PPITCH = {
-	.idx = 0x2701, .sidx = 0x03, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x2701, 0x03, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 /* CiA 402 */
 const il_reg_t IL_REG_CTL_WORD = {
-	.idx = 0x6040, .sidx = 0x00, .dtype = IL_REG_DTYPE_U16,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x6040, 0x00, IL_REG_DTYPE_U16, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_STS_WORD = {
-	.idx = 0x6041, .sidx = 0x00, .dtype = IL_REG_DTYPE_U16,
-	.access = IL_REG_ACCESS_RO, .phy = IL_REG_PHY_NONE
+	0x6041, 0x00, IL_REG_DTYPE_U16, IL_REG_ACCESS_RO, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_OP_MODE = {
-	.idx = 0x6060, .sidx = 0x00, .dtype = IL_REG_DTYPE_S8,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x6060, 0x00, IL_REG_DTYPE_S8, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_POS_ACT = {
-	.idx = 0x6064, .sidx = 0x00, .dtype = IL_REG_DTYPE_S32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_POS
+	0x6064, 0x00, IL_REG_DTYPE_S32, IL_REG_ACCESS_RW, IL_REG_PHY_POS
 };
 
 const il_reg_t IL_REG_VEL_ACT = {
-	.idx = 0x606C, .sidx = 0x00, .dtype = IL_REG_DTYPE_S32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_VEL
+	0x606C, 0x00, IL_REG_DTYPE_S32, IL_REG_ACCESS_RW, IL_REG_PHY_VEL
 };
 
 const il_reg_t IL_REG_TORQUE_TGT = {
-	.idx = 0x6071, .sidx = 0x00, .dtype = IL_REG_DTYPE_S16,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_TORQUE
+	0x6071, 0x00, IL_REG_DTYPE_S16, IL_REG_ACCESS_RW, IL_REG_PHY_TORQUE
 };
 
 const il_reg_t IL_REG_RATED_TORQUE = {
-	.idx = 0x6076, .sidx = 0x00, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x6076, 0x00, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_TORQUE_ACT = {
-	.idx = 0x6077, .sidx = 0x00, .dtype = IL_REG_DTYPE_S16,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_TORQUE
+	0x6077, 0x00, IL_REG_DTYPE_S16, IL_REG_ACCESS_RW, IL_REG_PHY_TORQUE
 };
 
 const il_reg_t IL_REG_POS_TGT = {
-	.idx = 0x607A, .sidx = 0x00, .dtype = IL_REG_DTYPE_S32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_POS
+	0x607A, 0x00, IL_REG_DTYPE_S32, IL_REG_ACCESS_RW, IL_REG_PHY_POS
 };
 
 const il_reg_t IL_REG_PRES_ENC_INCR = {
-	.idx = 0x608F, .sidx = 0x01, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x608F, 0x01, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_PRES_MOTOR_REVS = {
-	.idx = 0x608F, .sidx = 0x02, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x608F, 0x02, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_VRES_ENC_INCR = {
-	.idx = 0x6090, .sidx = 0x01, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x6090, 0x01, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_VRES_MOTOR_REVS = {
-	.idx = 0x6090, .sidx = 0x02, .dtype = IL_REG_DTYPE_U32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_NONE
+	0x6090, 0x02, IL_REG_DTYPE_U32, IL_REG_ACCESS_RW, IL_REG_PHY_NONE
 };
 
 const il_reg_t IL_REG_VEL_TGT = {
-	.idx = 0x60FF, .sidx = 0x00, .dtype = IL_REG_DTYPE_S32,
-	.access = IL_REG_ACCESS_RW, .phy = IL_REG_PHY_VEL
+	0x60FF, 0x00, IL_REG_DTYPE_S32, IL_REG_ACCESS_RW, IL_REG_PHY_VEL
 };
