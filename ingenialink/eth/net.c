c92db353455d62f4d4b27c061f6b6c51ea45b004
Oriol Burgaya <oburgaya@ingeniamc.com>
2019-04-09 12:45:38 +0200

    SWLIB-105 Test with network status

--- ingenialink/eth/net.c
+++ ingenialink/eth/net.c
@@ -563,6 +563,7 @@ il_eth_net_dev_list_t *il_eth_net_dev_list_get()
 static int il_eth_status_get(il_net_t *net)
 {
 	il_eth_net_t *this = to_eth_net(net);
+	printf("Status\n");
 	return this->stop;
 }
 
