259c2dbc93aaf8e8c79e0b002b58b992360a323a
rgomez-ingeniamc <rgomez@ingeniamc.com>
2020-01-22 15:18:16 +0100

    Fix state change status word on enable/disable subnodes

--- ingenialink/base/servo.c
+++ ingenialink/base/servo.c
@@ -287,10 +287,14 @@ static void sw_update(void *ctx, uint16_t sw)
  */
 static int state_subs_monitor(void *args)
 {
-	// STATUS WORD
+	// Init internal variables
+	uint16_t sw;
+	uint8_t subnode = 1;
+	il_servo_t *servo = args;
+	// Status word register
 	il_reg_t status_word_register = {
-		.subnode = 1,
-		.address = 0x0010,
+		.subnode = subnode,
+		.address = 0x0011,
 		.dtype = IL_REG_DTYPE_U16,
 		.access = IL_REG_ACCESS_RW,
 		.phy = IL_REG_PHY_NONE,
@@ -302,44 +306,46 @@ static int state_subs_monitor(void *args)
 		.enums = NULL,
 		.enums_count = 0
 	};
-	il_servo_t *servo = args;
-	il_servo_state_t states[5] = { IL_SERVO_STATE_DISABLED };
+	il_servo_state_t states[5] = { IL_SERVO_STATE_NRDY, IL_SERVO_STATE_NRDY, IL_SERVO_STATE_NRDY, IL_SERVO_STATE_NRDY, IL_SERVO_STATE_NRDY };
 	Sleep(200);
 	while (!servo->state_subs.stop) {
 		for (uint8_t i = 0; i < servo->subnodes; i++) {
-			uint8_t subnode = i + 1;
-			double sw;
+			subnode = i + 1;
 			status_word_register.subnode = subnode;
 			osal_mutex_lock(servo->sw.lock);
-			(void)il_servo_read(servo, &status_word_register, NULL, &sw);
-			if (servo->sw.value != sw) {
-				servo->sw.value = sw;
-				osal_cond_broadcast(servo->sw.changed);
+			int r = il_servo_raw_read_u16(servo, &status_word_register, NULL, &sw);
+			if (r < 0) {
+				osal_mutex_unlock(servo->sw.lock);
 			}
-			osal_mutex_unlock(servo->sw.lock);
-			/* obtain state/flags */
-			il_servo_state_t current_state;
-			int flags;
-			servo->ops->_state_decode(sw, &current_state, &flags);
-			if (current_state != states[i]) {
-				states[i] = current_state;
-				/* notify all subscribers */
-				size_t sz;
-				osal_mutex_lock(servo->state_subs.lock);
-				for (sz = 0; sz < servo->state_subs.sz; sz++) {
-					void *ctx;
-					if (!servo->state_subs.subs[sz].cb)
-						continue;
-					ctx = servo->state_subs.subs[sz].ctx;
-					servo->state_subs.subs[sz].cb(ctx, current_state, flags, subnode);
+			else {
+				if (servo->sw.value != sw) {
+					servo->sw.value = sw;
+					osal_cond_broadcast(servo->sw.changed);
 				}
+				osal_mutex_unlock(servo->sw.lock);
+				/* obtain state/flags */
+				il_servo_state_t current_state;
+				int flags;
+				servo->ops->_state_decode(sw, &current_state, &flags);
+				if (current_state != states[i]) {
+					states[i] = current_state;
+					/* notify all subscribers */
+					size_t sz;
+					osal_mutex_lock(servo->state_subs.lock);
+					for (sz = 0; sz < servo->state_subs.sz; sz++) {
+						void *ctx;
+						if (!servo->state_subs.subs[sz].cb)
+							continue;
+						ctx = servo->state_subs.subs[sz].ctx;
+						servo->state_subs.subs[sz].cb(ctx, current_state, flags, subnode);
+					}
 
-				osal_mutex_unlock(servo->state_subs.lock);
-			}		
-			Sleep(200);
+					osal_mutex_unlock(servo->state_subs.lock);	
+				}
+			}
 		}
+		Sleep(200);
 	}
-
 	return 0;
 }
 
