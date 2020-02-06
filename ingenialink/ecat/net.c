73979385524641be1e16e6e0cf4f9b44b630c14a
Oriol Burgaya <oriol.burgaya@celeramotion.com>
2020-02-06 09:36:44 +0100

    Quick fix to return correct error codes in EEPROM tool and FoE

--- ingenialink/ecat/net.c
+++ ingenialink/ecat/net.c
@@ -1343,11 +1343,14 @@ static int *il_ecat_net_update_firmware(il_net_t **net, char *ifname, uint16_t s
 							ec_statecheck(slave, EC_STATE_INIT, EC_TIMEOUTSTATE * 4);
 							printf("Slave %d state to INIT.\n", slave);
 						}
-						else
+						else 
+						{
 							printf("File not read OK.\n");
 							r = -1;
+						}	
 					}
-					else {
+					else 
+					{
 						printf("BOOT state not reached.\n");
 						r = -1;
 					}
@@ -1688,85 +1691,90 @@ static int *il_ecat_net_eeprom_tool(il_net_t **net, char *ifname, int slave, int
 			{
 				if ((mode == MODE_INFO) || (mode == MODE_READBIN) || (mode == MODE_READINTEL))
 				{
-				tstart = osal_current_time();
-				eeprom_read(slave, 0x0000, MINBUF); // read first 128 bytes
+					tstart = osal_current_time();
+					eeprom_read(slave, 0x0000, MINBUF); // read first 128 bytes
 
-				wbuf = (uint16 *)&ebuf[0];
-				printf("Slave %d data\n", slave);
-				printf(" PDI Control      : %4.4X\n",*(wbuf + 0x00));
-				printf(" PDI Config       : %4.4X\n",*(wbuf + 0x01));
-				printf(" Config Alias     : %4.4X\n",*(wbuf + 0x04));
-				printf(" Checksum         : %4.4X\n",*(wbuf + 0x07));
-				printf("   calculated     : %4.4X\n",SIIcrc(&ebuf[0]));
-				printf(" Vendor ID        : %8.8X\n",*(uint32 *)(wbuf + 0x08));
-				printf(" Product Code     : %8.8X\n",*(uint32 *)(wbuf + 0x0A));
-				printf(" Revision Number  : %8.8X\n",*(uint32 *)(wbuf + 0x0C));
-				printf(" Serial Number    : %8.8X\n",*(uint32 *)(wbuf + 0x0E));
-				printf(" Mailbox Protocol : %4.4X\n",*(wbuf + 0x1C));
-				esize = (*(wbuf + 0x3E) + 1) * 128;
-				if (esize > MAXBUF) esize = MAXBUF;
-				printf(" Size             : %4.4X = %d bytes\n",*(wbuf + 0x3E), esize);
-				printf(" Version          : %4.4X\n",*(wbuf + 0x3F));
-				}
-				if ((mode == MODE_READBIN) || (mode == MODE_READINTEL))
-				{
-				if (esize > MINBUF)
-					eeprom_read(slave, MINBUF, esize - MINBUF); // read reminder
-
-				tend = osal_current_time();
-				osal_time_diff(&tstart, &tend, &tdif);
-				if (mode == MODE_READINTEL) output_intelhex(fname, esize);
-				if (mode == MODE_READBIN)   output_bin(fname, esize);
-
-				printf("\nTotal EEPROM read time :%ldms\n", (tdif.usec+(tdif.sec*1000000L)) / 1000);
-				}
-				if ((mode == MODE_WRITEBIN) || (mode == MODE_WRITEINTEL))
-				{
-				estart = 0;
-				if (mode == MODE_WRITEINTEL) rc = input_intelhex(fname, &estart, &esize);
-				if (mode == MODE_WRITEBIN)   rc = input_bin_eeprom(fname, &esize);
-
-				if (rc > 0)
-				{
 					wbuf = (uint16 *)&ebuf[0];
-					printf("Slave %d\n", slave);
+					printf("Slave %d data\n", slave);
+					printf(" PDI Control      : %4.4X\n",*(wbuf + 0x00));
+					printf(" PDI Config       : %4.4X\n",*(wbuf + 0x01));
+					printf(" Config Alias     : %4.4X\n",*(wbuf + 0x04));
+					printf(" Checksum         : %4.4X\n",*(wbuf + 0x07));
+					printf("   calculated     : %4.4X\n",SIIcrc(&ebuf[0]));
 					printf(" Vendor ID        : %8.8X\n",*(uint32 *)(wbuf + 0x08));
 					printf(" Product Code     : %8.8X\n",*(uint32 *)(wbuf + 0x0A));
 					printf(" Revision Number  : %8.8X\n",*(uint32 *)(wbuf + 0x0C));
 					printf(" Serial Number    : %8.8X\n",*(uint32 *)(wbuf + 0x0E));
+					printf(" Mailbox Protocol : %4.4X\n",*(wbuf + 0x1C));
+					esize = (*(wbuf + 0x3E) + 1) * 128;
+					if (esize > MAXBUF) esize = MAXBUF;
+					printf(" Size             : %4.4X = %d bytes\n",*(wbuf + 0x3E), esize);
+					printf(" Version          : %4.4X\n",*(wbuf + 0x3F));
+				}
+				if ((mode == MODE_READBIN) || (mode == MODE_READINTEL))
+				{
+					if (esize > MINBUF) 
+					{
+						eeprom_read(slave, MINBUF, esize - MINBUF); // read reminder
+					}
+						
 
-					printf("Busy");
-					fflush(stdout);
-					tstart = osal_current_time();
-					eeprom_write(slave, estart, esize);
 					tend = osal_current_time();
 					osal_time_diff(&tstart, &tend, &tdif);
+					if (mode == MODE_READINTEL) output_intelhex(fname, esize);
+					if (mode == MODE_READBIN)   output_bin(fname, esize);
 
-					printf("\nTotal EEPROM write time :%ldms\n", (tdif.usec+(tdif.sec*1000000L)) / 1000);
+					printf("\nTotal EEPROM read time :%ldms\n", (tdif.usec+(tdif.sec*1000000L)) / 1000);
 				}
-				else
-					printf("Error reading file, abort.\n");
-					r = -1;
-				}
-				if (mode == MODE_WRITEALIAS)
+				if ((mode == MODE_WRITEBIN) || (mode == MODE_WRITEINTEL))
 				{
-				if( eeprom_read(slave, 0x0000, CRCBUF) ) // read first 14 bytes
-				{
-					wbuf = (uint16 *)&ebuf[0];
-					*(wbuf + 0x04) = alias;
-					if(eeprom_writealias(slave, alias, SIIcrc(&ebuf[0])))
+					estart = 0;
+					if (mode == MODE_WRITEINTEL) rc = input_intelhex(fname, &estart, &esize);
+					if (mode == MODE_WRITEBIN)   rc = input_bin_eeprom(fname, &esize);
+
+					if (rc > 0)
 					{
-						printf("Alias %4.4X written successfully to slave %d\n", alias, slave);
+						wbuf = (uint16 *)&ebuf[0];
+						printf("Slave %d\n", slave);
+						printf(" Vendor ID        : %8.8X\n", *(uint32 *)(wbuf + 0x08));
+						printf(" Product Code     : %8.8X\n", *(uint32 *)(wbuf + 0x0A));
+						printf(" Revision Number  : %8.8X\n", *(uint32 *)(wbuf + 0x0C));
+						printf(" Serial Number    : %8.8X\n", *(uint32 *)(wbuf + 0x0E));
+
+						printf("Busy");
+						fflush(stdout);
+						tstart = osal_current_time();
+						eeprom_write(slave, estart, esize);
+						tend = osal_current_time();
+						osal_time_diff(&tstart, &tend, &tdif);
+
+						printf("\nTotal EEPROM write time :%ldms\n", (tdif.usec + (tdif.sec * 1000000L)) / 1000);
 					}
 					else
 					{
-						printf("Alias not written\n");
+						printf("Error reading file, abort.\n");
+						r = -1;
 					}
 				}
-				else
+				if (mode == MODE_WRITEALIAS)
 				{
-					printf("Could not read slave EEPROM");
-				}
+					if( eeprom_read(slave, 0x0000, CRCBUF) ) // read first 14 bytes
+					{
+						wbuf = (uint16 *)&ebuf[0];
+						*(wbuf + 0x04) = alias;
+						if(eeprom_writealias(slave, alias, SIIcrc(&ebuf[0])))
+						{
+							printf("Alias %4.4X written successfully to slave %d\n", alias, slave);
+						}
+						else
+						{
+							printf("Alias not written\n");
+						}
+					}
+					else
+					{
+						printf("Could not read slave EEPROM");
+					}
 				}
 			}
 			else
