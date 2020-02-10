6ce59a3e0bdf19ba6da0b61ddb349f4670a39bd4
rgomez-ingeniamc <rgomez@ingeniamc.com>
2020-02-10 16:40:54 +0100

    Initialize correctly subnodes attribute for MCB devices

--- ingenialink/mcb/servo.c
+++ ingenialink/mcb/servo.c
@@ -257,6 +257,17 @@ static il_servo_t *il_mcb_servo_create(il_net_t *net, uint16_t id,
 
 	this->servo.ops = &il_mcb_servo_ops;
 
+	
+	/* Configure the number of axis if the register is defined */
+	uint16_t subnodes;
+	r = il_servo_raw_read_u16(&this->servo, &IL_REG_ETH_NUMBER_AXIS, NULL, &subnodes);
+	if (r < 0) {
+		this->servo.subnodes = 1;
+	}
+	else {
+		this->servo.subnodes = subnodes;
+	}
+
 	/* initialize, setup refcnt */
 	this->refcnt = il_utils__refcnt_create(servo_destroy, this);
 	if (!this->refcnt)
