8053f62069a735bbefaa444e4996bc3a8c12df78
Oriol Burgaya <oburgaya@ingeniamc.com>
2020-10-02 11:32:04 +0200

    **il_net_t to *il_net_t

--- include/public/ingenialink/net.h
+++ include/public/ingenialink/net.h
@@ -615,7 +615,7 @@ IL_EXPORT int il_net_close_socket(il_net_t *net);
 */
 IL_EXPORT int il_net_master_startup(il_net_t **net, const char *ifname, const char *if_address_ip);
 
-IL_EXPORT int il_net_set_if_params(il_net_t **net, const char *ifname, const char *if_address_ip);
+IL_EXPORT int il_net_set_if_params(il_net_t *net, const char *ifname, const char *if_address_ip);
 
 IL_EXPORT int il_net_master_stop(il_net_t **net);
 
